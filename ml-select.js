dacedf527c4397e5fae37b7f292270a3099742eb
cbm94 <cbm94@cornell.edu>
2022-07-08 15:41:46 -0400

    some fixes, put script in oneditprepare function

--- ml-select.js
+++ ml-select.js
@@ -3,6 +3,7 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
         node.warn(config.retrigger_window)
+        node.warn(config.files)
         node.on('input', function(msg) {
             msg.payload = msg.payload.toLowerCase();
             node.send(msg);
